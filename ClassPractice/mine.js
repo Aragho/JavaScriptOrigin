16cdbe4ce6f73a47eb574b3cbfb24cef30119f7f
Aragho <Oluwadanny19@gmail.com>
2024-10-22 10:11:52 +0100

    code

--- ClassPractice/mine.js
+++ ClassPractice/mine.js
@@ -12,3 +12,10 @@ console.log(result)//  send to the serves as an object
 let data = '{"firstName":"adenike","lastName":"ara","sex":"female"}';
 let answer = JSON.parse(data);
 console.log(answer);
+// when ypur are calling a function outside a file #asyncronious programming
+//u can use async without await
+// allows you to continue another function while you wait for other function
+//async
+
+//you cant use awit without async
+//await
